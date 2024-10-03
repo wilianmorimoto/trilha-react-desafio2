697b0d2c0b8c941d2752078369bd0befa9146429
wilianmorimoto <wilianhmorimoto@gmail.com>
2024-10-03 13:21:33 -0300

    initial commit

---
+++ src/styles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+  *, body {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  body {
+    background: #22272e;
+    color: #ffffff;
+  }
+`
